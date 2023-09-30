57835bae9964a06d1393698128908cd636895194
unknown <arti0090@gmail.com>
2023-09-30 11:02:52 +0200

    File Manager: sort in asc and desc of files

--- file_manager/src/file_manager.rs
+++ file_manager/src/file_manager.rs
@@ -21,4 +21,38 @@ impl FileManager {
 
         Ok(())
     }
+
+    pub fn sort_files_in_directory(directory: &str, direction: &str) -> io::Result<()> {
+        let entries = fs::read_dir(directory)?;
+
+        let mut files_with_lengths: Vec<(String, u64)> = Vec::new();
+
+        for entry in entries {
+            let entry = entry?;
+            let path = entry.path();
+
+            // check for files and their metadata, push this data to Vector (array) of name and len
+            if path.is_file() {
+                if let Some(file_name) = path.file_name() {
+                    if let Some(file_name_str) = file_name.to_str() {
+                        if let Ok(metadata) = fs::metadata(&path) {
+                            files_with_lengths.push((file_name_str.to_string(), metadata.len()));
+                        }
+                    }
+                }
+            }
+
+            match direction {
+                // sort vector by the length in ascending or desc order
+                "DESC" => files_with_lengths.sort_by_key(|&(_, length)| std::cmp::Reverse(length)),
+                _ => files_with_lengths.sort_by_key(|&(_, length)| length),
+            }
+
+            for (file_name, length) in &files_with_lengths {
+                println!("{}: {}", file_name, length);
+            }
+        }
+
+        Ok(())
+    }
 }
--- file_manager/src/main.rs
+++ file_manager/src/main.rs
@@ -6,4 +6,8 @@ fn main() {
     let directory = "C:/Users/Administrator/Documents";
 
     let _ = FileManager::list_files_in_directory(directory);
+
+    // direction could be an Enum in future
+    let _ = FileManager::sort_files_in_directory(directory, "ASC");
+    let _ = FileManager::sort_files_in_directory(directory, "DESC");
 }
